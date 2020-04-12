9ae602bdb3e0ae42fda433ee77b2ddcb0d6f4632
ekalii <andoune0719@gmail.com>
2020-04-12 15:57:30 +0900

    make reducer function

---
+++ todolist/reducers.js
@@ -0,0 +1,41 @@
+import { 
+    VisibilityFilters,
+    SET_VISIBILITY_FILTER,
+    ADD_TODO,
+    TOGGLE_TODO
+} from './actions';
+
+const initialState = {
+    VisibilityFilters: VisibilityFilters.SHOW_ALL,
+    todos: [],
+}
+
+function todoApp(state = initialState, action) {
+    switch(action.type) {
+        case SET_VISIBILITY_FILTER:
+            return Object.assign({}, state, { VisibilityFilters: action.filter });
+        case ADD_TODO:
+            return Object.assign({}, state, {
+                todos: [
+                    ...state.todos,
+                    {
+                        text: action.text,
+                        completed: false
+                    }
+                ]
+            })
+        case TOGGLE_TODO:
+            return Object.assign({}, state, {
+                todos: state.todos.map((todo, index) => {
+                    if (index === action.index) {
+                        return Object.assign({}, todo, {
+                            completed: !todo.completed
+                        });
+                    };
+                    return todo;
+                })
+            });
+        default:
+            return state;
+    }
+}
